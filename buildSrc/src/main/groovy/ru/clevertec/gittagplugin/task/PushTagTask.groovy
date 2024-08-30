d28f279538700dbeaa3479de6065c2dfc19ff6b8
Nikolay1992167 <wulik1081@mail.ru>
2024-08-31 00:48:31 +0300

    fix: fix check5

--- buildSrc/src/main/groovy/ru/clevertec/gittagplugin/task/PushTagTask.groovy
+++ buildSrc/src/main/groovy/ru/clevertec/gittagplugin/task/PushTagTask.groovy
@@ -175,19 +175,47 @@ class PushTagTask extends DefaultTask {
         }
 
         def tags = execOutput.toString().trim().split('\n')
-        def incrementalTags = tags.findAll { it ==~ /v\d+\.\d+$/ }
+        def incrementalTags = tags.findAll { it ==~ /v\d+\.\d+(-\w+)?$/ }
         if (incrementalTags.isEmpty()) {
             return ""
         }
 
         incrementalTags.sort { a, b ->
-            def (aMajor, aMinor) = a.replace("v", "").split("\\.").collect { it.toInteger() }
-            def (bMajor, bMinor) = b.replace("v", "").split("\\.").collect { it.toInteger() }
+            def (aMajor, aMinor) = a.replaceAll(/v(\d+)\.(\d+).*/, '$1 $2').split(' ').collect { it.toInteger() }
+            def (bMajor, bMinor) = b.replaceAll(/v(\d+)\.(\d+).*/, '$1 $2').split(' ').collect { it.toInteger() }
             return aMajor <=> bMajor ?: aMinor <=> bMinor
         }
 
-        return incrementalTags.last()
+        def latestTag = incrementalTags.last()
+        return latestTag.replaceAll(/(-\w+)?$/, "")
     }
+
+//    private String findLatestIncrementTag() {
+//        def execOutput = new ByteArrayOutputStream()
+//        def result = project.exec {
+//            commandLine 'sh', '-c', "git tag --list 'v*'"
+//            standardOutput = execOutput
+//            errorOutput = new ByteArrayOutputStream()
+//            ignoreExitValue = true
+//        }
+//        if (result.exitValue != 0) {
+//            return ""
+//        }
+//
+//        def tags = execOutput.toString().trim().split('\n')
+//        def incrementalTags = tags.findAll { it ==~ /v\d+\.\d+$/ }
+//        if (incrementalTags.isEmpty()) {
+//            return ""
+//        }
+//
+//        incrementalTags.sort { a, b ->
+//            def (aMajor, aMinor) = a.replace("v", "").split("\\.").collect { it.toInteger() }
+//            def (bMajor, bMinor) = b.replace("v", "").split("\\.").collect { it.toInteger() }
+//            return aMajor <=> bMajor ?: aMinor <=> bMinor
+//        }
+//
+//        return incrementalTags.last()
+//    }
 
     private String findCurrentBranchName() {
         def execOutput = new ByteArrayOutputStream()
