775a3690ef064d1329e210bb9836aeb4541716bd
Nikolay1992167 <wulik1081@mail.ru>
2024-08-31 01:04:53 +0300

    fix: fix check9

--- src/main/java/ru/clevertec/Main.java
+++ src/main/java/ru/clevertec/Main.java
@@ -2,6 +2,6 @@ package ru.clevertec;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Checking the plugin's operation");
+        System.out.println("Checking the plugin's operation!");
     }
 }
