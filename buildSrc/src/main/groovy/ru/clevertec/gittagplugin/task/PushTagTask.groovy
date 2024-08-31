4aaa7b7a1030d5da8aeb15030204d7eee7600719
Nikolay1992167 <wulik1081@mail.ru>
2024-08-31 15:29:23 +0300

    fix: fixed functionality git-tag-plugin

--- buildSrc/src/main/groovy/ru/clevertec/gittagplugin/task/PushTagTask.groovy
+++ buildSrc/src/main/groovy/ru/clevertec/gittagplugin/task/PushTagTask.groovy
@@ -5,123 +5,70 @@ import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
 import ru.clevertec.gittagplugin.model.Branch
 import ru.clevertec.gittagplugin.service.ExistTagService
+import ru.clevertec.gittagplugin.service.ProcessTagService
 import ru.clevertec.gittagplugin.service.NoTagService
 
-import java.util.regex.Matcher
-import java.util.regex.Pattern
-
-import static ru.clevertec.gittagplugin.util.Constants.*
+import static ru.clevertec.gittagplugin.util.Constants.TAG
 
 class PushTagTask extends DefaultTask {
-
     @Input
     def noTagService = new NoTagService()
 
     @Input
     def existTagService = new ExistTagService()
 
+    @Input
+    def gitTagService = new ProcessTagService(project)
+
     @TaskAction
     void pushTag() {
-        def branchName = findCurrentBranchName()
+        def branchName = gitTagService.findCurrentBranchName()
         def tagFromDefineTagTask = project.extensions.getByName(TAG).toString()
 
         if (tagFromDefineTagTask.empty) {
-
-            if (findGitTags().isEmpty()) {
+            if (gitTagService.findGitTags().isEmpty()) {
                 def tagTitle = noTagService.createTagName(branchName, tagFromDefineTagTask)
-                pushTagToLocal(tagTitle)
-                pushTagToOrigin(tagTitle)
+                pushTagVersion(tagTitle)
             } else {
-                if (branchName.equalsIgnoreCase("master")) {
-                    def latestTag = findLatestTagsInBranch(branchName)
-                    def tagTitle = existTagService.createTagName(branchName, latestTag)
-                    pushTagToLocal(tagTitle)
-                    pushTagToOrigin(tagTitle)
-                } else {
-                    def latestTagVersion = findLatestIncrementTag()
-                    def preliminaryTag = setPostfix(latestTagVersion, branchName)
-                    def fixedTag = updateMajorVersionIfNotMaster(branchName, preliminaryTag)
-                    def tagTitle = existTagService.createTagName(branchName, fixedTag)
-                    pushTagToLocal(tagTitle)
-                    pushTagToOrigin(tagTitle)
-                }
+                setTagAccordingBranch(branchName, gitTagService)
             }
         } else {
-            print tagFromDefineTagTask
+            print "Tag is already installed: " + tagFromDefineTagTask
         }
     }
 
-    private String updateMajorVersionIfNotMaster(String branchName, String latestTagVersion) {
+    void setTagAccordingBranch(String branchName, ProcessTagService gitTagService) {
+        if (branchName.equalsIgnoreCase(Branch.MASTER.toString())) {
+            def latestTag = gitTagService.findLatestTagsInBranch(branchName)
+            def tagTitle = existTagService.createTagName(branchName, latestTag)
+            pushTagVersion(tagTitle)
+        } else {
+            def latestTagVersion = gitTagService.findLatestIncrementTag()
+            def preliminaryTag = ProcessTagService.setPostfix(latestTagVersion, branchName)
+            def fixedTag = updateMajorVersionIfNotMaster(branchName, preliminaryTag, gitTagService)
+            def tagTitle = existTagService.createTagName(branchName, fixedTag)
+            pushTagVersion(tagTitle)
+        }
+    }
+
+    private static String updateMajorVersionIfNotMaster(String branchName, String latestTagVersion, ProcessTagService gitTagService) {
         if (branchName != Branch.MASTER.toString()) {
-            def lastTagMaster = findLatestTagMajorVersion()
-            def majorVersion = extractMajorVersion(lastTagMaster)
-            return setMajorVersion(latestTagVersion, majorVersion)
+            def lastTagMaster = gitTagService.findLatestTagMajorVersion()
+            def majorVersion = ProcessTagService.extractMajorVersion(lastTagMaster)
+            return ProcessTagService.setMajorVersion(latestTagVersion, majorVersion)
         }
         return latestTagVersion
     }
 
-    private String findLatestTagMajorVersion() {
-        List<String> tags = findGitTags()
-
-        int maxMajorVersion = -1;
-        String latestTag = "";
-
-        for (String tag : tags) {
-            int majorVersion = extractMajorVersion(tag);
-            if (majorVersion > maxMajorVersion) {
-                maxMajorVersion = majorVersion;
-                latestTag = tag;
-            }
-        }
-        return latestTag;
-    }
-
-    private List<String> findGitTags() {
-        def execOutput = new ByteArrayOutputStream()
-        def result = project.exec {
-            commandLine GIT, TAG
-            standardOutput = execOutput
-            errorOutput = new ByteArrayOutputStream()
-            ignoreExitValue = true
-        }
-        if (result.exitValue != 0) {
-            return Collections.emptyList()
-        }
-        def tags = execOutput.toString().trim().split('\n').toList()
-        return tags.findAll {it.trim()}
-    }
-
-//    private List<String> findGitTags() {
-//        def execOutput = new ByteArrayOutputStream()
-//        def result = project.exec {
-//            commandLine GIT, TAG
-//            standardOutput = execOutput
-//            errorOutput = new ByteArrayOutputStream()
-//            ignoreExitValue = true
-//        }
-//        if (result.exitValue != 0) {
-//            return Collections.emptyList()
-//        }
-//        return execOutput.toString().trim().split('\n').toList()
-//    }
-
-    private static int extractMajorVersion(String tag) {
-        Pattern pattern = Pattern.compile("v(\\d+)\\.\\d+");
-        Matcher matcher = pattern.matcher(tag);
-        if (matcher.matches()) {
-            return Integer.parseInt(matcher.group(1));
-        }
-        return 0;
-    }
-
-    private static String setMajorVersion(String tag, Integer majorVersion) {
-        return tag.replaceAll("v\\d+\\.\\d+", "v" + majorVersion + ".0");
+    private void pushTagVersion(String tagTitle) {
+        pushTagToLocal(tagTitle)
+        pushTagToOrigin(tagTitle)
     }
 
     private void pushTagToLocal(String tagTitle) {
         def execOutput = new ByteArrayOutputStream()
         project.exec {
-            commandLine GIT, TAG, tagTitle
+            commandLine 'git', 'tag', tagTitle
             standardOutput = execOutput
         }
         print "The current commit is assigned tag $tagTitle"
@@ -130,101 +77,8 @@ class PushTagTask extends DefaultTask {
     private void pushTagToOrigin(String tagTitle) {
         def execOutput = new ByteArrayOutputStream()
         project.exec {
-            commandLine GIT, PUSH, ORIGIN, tagTitle
+            commandLine 'git', 'push', 'origin', tagTitle
             standardOutput = execOutput
         }
-    }
-
-    private static String setPostfix(String latestTagVersion, String branchName) {
-        String postfix = ""
-
-        if (branchName.equalsIgnoreCase("stage")) {
-            postfix = "-rc"
-        } else if (!branchName.equalsIgnoreCase("master") &&
-                !branchName.equalsIgnoreCase("dev") &&
-                !branchName.equalsIgnoreCase("qa")) {
-            postfix = "-SNAPSHOT"
-        }
-        return latestTagVersion + postfix
-    }
-
-    private String findLatestTagsInBranch(String nameBranch) {
-        def execOutput = new ByteArrayOutputStream()
-        def result = project.exec {
-            commandLine 'sh', '-c', "git describe --tags \$(git rev-list --tags --max-count=1 --branches=${nameBranch})"
-            standardOutput = execOutput
-            errorOutput = new ByteArrayOutputStream()
-            ignoreExitValue = true
-        }
-        if (result.exitValue != 0) {
-            return ""
-        }
-        return execOutput.toString()
-    }
-
-    private String findLatestIncrementTag() {
-        def execOutput = new ByteArrayOutputStream()
-        def result = project.exec {
-            commandLine 'sh', '-c', "git tag --list 'v*'"
-            standardOutput = execOutput
-            errorOutput = new ByteArrayOutputStream()
-            ignoreExitValue = true
-        }
-        if (result.exitValue != 0) {
-            return ""
-        }
-
-        def tags = execOutput.toString().trim().split('\n')
-        def incrementalTags = tags.findAll { it ==~ /v\d+\.\d+(-\w+)?$/ }
-        if (incrementalTags.isEmpty()) {
-            return ""
-        }
-
-        incrementalTags.sort { a, b ->
-            def (aMajor, aMinor) = a.replaceAll(/v(\d+)\.(\d+).*/, '$1 $2').split(' ').collect { it.toInteger() }
-            def (bMajor, bMinor) = b.replaceAll(/v(\d+)\.(\d+).*/, '$1 $2').split(' ').collect { it.toInteger() }
-            return aMajor <=> bMajor ?: aMinor <=> bMinor
-        }
-
-        def latestTag = incrementalTags.last()
-        return latestTag.replaceAll(/(-\w+)?$/, "")
-    }
-
-//    private String findLatestIncrementTag() {
-//        def execOutput = new ByteArrayOutputStream()
-//        def result = project.exec {
-//            commandLine 'sh', '-c', "git tag --list 'v*'"
-//            standardOutput = execOutput
-//            errorOutput = new ByteArrayOutputStream()
-//            ignoreExitValue = true
-//        }
-//        if (result.exitValue != 0) {
-//            return ""
-//        }
-//
-//        def tags = execOutput.toString().trim().split('\n')
-//        def incrementalTags = tags.findAll { it ==~ /v\d+\.\d+$/ }
-//        if (incrementalTags.isEmpty()) {
-//            return ""
-//        }
-//
-//        incrementalTags.sort { a, b ->
-//            def (aMajor, aMinor) = a.replace("v", "").split("\\.").collect { it.toInteger() }
-//            def (bMajor, bMinor) = b.replace("v", "").split("\\.").collect { it.toInteger() }
-//            return aMajor <=> bMajor ?: aMinor <=> bMinor
-//        }
-//
-//        return incrementalTags.last()
-//    }
-
-    private String findCurrentBranchName() {
-        def execOutput = new ByteArrayOutputStream()
-        project.exec {
-            commandLine GIT, BRANCH, SHOW_CURRENT
-            standardOutput = execOutput
-            errorOutput = new ByteArrayOutputStream()
-            ignoreExitValue = true
-        }
-        return execOutput.toString().trim()
     }
 }
